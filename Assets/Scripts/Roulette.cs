c27b17d839ad7df1cc43c3e69f28bdf0f517c475
Kyoungjoon-Virnect <hoab@virnect.com>
2023-12-19 00:14:32 +0900

    룰렛 한바퀴 도는 로직 추가

--- Assets/Scripts/Roulette.cs
+++ Assets/Scripts/Roulette.cs
@@ -8,14 +8,31 @@ public class Roulette : MonoBehaviour
 {
     [SerializeField] Button spinButton;
 
+    private float totalRotationTime = 3f;
+    private float initalRotation = 0.0f;
+    
     private void Start()
     {
-        spinButton.onClick.AddListener(Spin);
+        spinButton?.onClick.AddListener(() => StartCoroutine(Spin()));
     }
 
-    private void Spin()
+    IEnumerator Spin()
     {
-        Debug.Log("Start Spin");
+        Debug.Log("kkj run spin");
+        float startTime = Time.time;
+
+        while(Time.time - startTime < totalRotationTime)
+        {
+            float runTime = Time.time - startTime;
+            float rotationAmount = Mathf.Lerp(0.0f, 360.0f, runTime / totalRotationTime);
+            transform.rotation = Quaternion.Euler(0, 0, -rotationAmount);
+
+            //한 프레임 대기
+            yield return null;
+        }
+
+        // 회전 끝나면 초기화
+        transform.rotation = Quaternion.Euler(0, 0, 0f);
     }
 
     public void SetData(int typeCount, List<string> typeList)
